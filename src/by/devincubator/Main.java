2d21572e3e131c5ab29676dff4c2d31b62bbc1eb
Anton Novosad <noobito.2011@gmail.com>
2022-06-28 17:04:15 +0300

    added solution of Level 1;

--- src/by/devincubator/Main.java
+++ src/by/devincubator/Main.java
@@ -1,7 +1,67 @@
 package by.devincubator;
 
+import by.devincubator.entity.VehicleType;
+
 public class Main {
     public static void main(String[] args) {
-        
+        VehicleType bus = new VehicleType("Bus", 1.2);
+        VehicleType car = new VehicleType("Car", 1);
+        VehicleType rink = new VehicleType("Rink", 1.5);
+        VehicleType tractor = new VehicleType("Tractor", 1.2);
+        // 1 point
+        VehicleType[] types = new VehicleType[]{bus, car, rink, tractor};
+        // 2 point
+        showTypes(types);
+        // 3 point
+        setCoefficientInTypes(types);
+        // 4 point
+        findMaxCoefficient(types);
+        // 5 point
+        findAverageCoefficient(types);
+        // 6 point
+        competeAllPoints(types);
+
+    }
+
+    private static void showTypes(VehicleType[] types) {
+        for (VehicleType v : types) {
+            System.out.println(v.display());
+        }
+    }
+
+    private static void setCoefficientInTypes(VehicleType[] types) {
+        for (int i = 0; i < types.length; i++) {
+            if (i == types.length - 1) {
+                types[i].setTaxCoefficient(1.3);
+            }
+        }
+    }
+
+    private static void findMaxCoefficient(VehicleType[] types) {
+        double max = 0;
+        for (int i = 0; i < types.length; i++) {
+            if (types[i].getTaxCoefficient() > max) {
+                max = types[i].getTaxCoefficient();
+            }
+        }
+        System.out.println(max);
+    }
+
+    private static void findAverageCoefficient(VehicleType[] types) {
+        double average = 0;
+        for (int i = 0; i < types.length; i++) {
+            average += types[i].getTaxCoefficient();
+        }
+        System.out.println(average / types.length);
+    }
+
+    private static void competeAllPoints(VehicleType[] types) {
+        boolean isComplete = true;
+        while (isComplete) {
+            showTypes(types);
+            findMaxCoefficient(types);
+            findAverageCoefficient(types);
+            isComplete = false;
+        }
     }
 }
---
+++ src/by/devincubator/entity/VehicleType.java
@@ -0,0 +1,41 @@
+package by.devincubator.entity;
+
+public class VehicleType {
+    private String name;
+    private double taxCoefficient;
+
+    public VehicleType() {
+    }
+
+    public VehicleType(String name, double taxCoefficient) {
+        this.name = name;
+        this.taxCoefficient = taxCoefficient;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getTaxCoefficient() {
+        return taxCoefficient;
+    }
+
+    public void setTaxCoefficient(double taxCoefficient) {
+        this.taxCoefficient = taxCoefficient;
+    }
+
+    public String display() {
+        return "Type name = " + name + '\n' +
+                "Tax coefficient = " + taxCoefficient;
+    }
+
+    @Override
+    public String toString() {
+        return name +
+                "," + '\"' + taxCoefficient + '\"';
+    }
+}
